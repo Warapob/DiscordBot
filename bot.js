fc10cc36d332cd7fb4ea488fb7e70c96f265eb6e
Warapob <warapob2544@gmail.com>
2021-05-29 17:15:47 +0700

    discord bot beta version 0.0.0.2

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const last2msg = [" ", " "];
+var voiceChannelMemberCount = 0;
+//sever id : 367646145834385408
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -32,15 +34,31 @@ client.on('message', msg => {
     console.log(" ");
 })
 
-
+//role Tier :  ขาประจำ id : 848123102743363616
+var tagStage = true
 client.on("voiceStateUpdate", (oldVoiceState, newVoiceState) => { // Listeing to the voiceStateUpdate event
     console.log("########## update voice channel log ##########\n");
     if (newVoiceState.channel) { // The member connected to a channel
         console.log(`${newVoiceState.member.user.tag} connected to ${newVoiceState.channel.name}.`);
+        ++voiceChannelMemberCount;
+        console.log("membere(Tier :  ขาประจำ role) in voice chanel : "+voiceChannelMemberCount);
+        //console.log(newVoiceState.member.roles.cache.get('848123102743363616'));
+        if (newVoiceState.member.roles.cache.get('848123102743363616')!=undefined&&voiceChannelMemberCount == 1 && tagStage == true){
+            tagStage = false;
+            setTimeout(function(){
+                client.channels.cache.get("719848875472715877").send("<@&848123102743363616>" + "เข้ามาเร็วๆดิ๊อย่าช้า");
+            },4000);
+        }
     }
     else if (oldVoiceState.channel) { // The member disconnected from a channel.
         console.log(`${oldVoiceState.member.user.tag} disconnected from ${oldVoiceState.channel.name}.`)
-    };
+        --voiceChannelMemberCount;
+        if(voiceChannelMemberCount <1){
+            voiceChannelMemberCount=0;
+            tagStage = true;
+        }
+    }
+    console.log(" ");
 });
 
 
